6f7e294e2db11be7ce07d7cb608caeb874fb58ce
lee-dong-gi <oksk327@naver.com>
2023-05-09 21:23:35 +0900

    capter_07 hook

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './cahpter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 )
 
